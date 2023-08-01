f9546606bd0c1c8160a37d02cc86287b5124fd24
Nadir Abbas <nadirabbasforwork@gmail.com>
2023-08-01 22:41:52 +0500

    test

--- src/socks5.js
+++ src/socks5.js
@@ -298,8 +298,8 @@ class SocksServer {
 					})
 					.word16bu("dst.port")
 					.tap((args) => {
-						console.log(args)
 						if (args.cmd === RFC_1928_COMMANDS.UDP_ASSOCIATE) {
+							console.log(args)
 							handleUdp(socket, args);
 						}
 
