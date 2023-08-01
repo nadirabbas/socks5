c9447e59f2e01e3d1e2bf9f686d6d82c46020a95
Nadir Abbas <nadirabbasforwork@gmail.com>
2023-08-01 21:46:31 +0500

    fix

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ const PORT = 6801
 const HOST = '0.0.0.0'
 
 fetch('https://api.ipify.org/').then(res => res.text()).then((ip) => {
-  socks5.createServer({ ip: ip.replace('\\n', '') }).listen(PORT, HOST);
+  socks5.createServer({ ip }).listen(PORT, HOST);
 })
